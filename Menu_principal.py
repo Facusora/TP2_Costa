c4734652d40900ed3b5f92f6cbbead202d46e7c4
Francisco Lopez <50122038+franlopez1234@users.noreply.github.com>
2021-07-05 18:28:02 -0300

    Subo el menu

---
+++ Menu_principal.py
@@ -0,0 +1,11 @@
+opcion=0
+while opcion !=8:
+    print("-----MENU PRINCIPAL-----")
+    print("1. Listar archivos de la carpeta actual")
+    print("2. Crear un archivo")
+    print("3. Subir un archivo.")
+    print("4. Descargar un archivo.")
+    print("5. Sincronizar.")
+    print("6. Generar carpetas de una evaluaci´on.")
+    print("7. Actualizar entregas de alumnos v´ıa mail.")
+    print("8. Salir.")

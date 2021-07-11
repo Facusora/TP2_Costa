ce988728e3446ec6fbcadf984b553676bd3fad80
franlopez1234 <frlopez@fi.uba.ar>
2021-07-11 18:21:47 -0300

    Validacion al menu

--- Menu_principal.py
+++ Menu_principal.py
@@ -1,3 +1,18 @@
+def validar_menu() -> int:
+    '''
+    Pre: Pide un numero en un rango
+    Post: Te responde si el numero esta en el rango
+    '''
+    numero = input("Ingrese una opcion: ")                        
+    while not numero.isnumeric() or (int(numero)) >8 or (int(numero)) <1 :
+        print("")
+        print("Esa opcion no es valida!")
+        print("")
+        numero = input("Ingrese una opcion valida:")
+
+    return int(numero)
+
+
 opcion=0
 while opcion !=8:
     print("-----MENU PRINCIPAL-----")
@@ -9,5 +24,26 @@ while opcion !=8:
     print("6. Generar carpetas de una evaluaci´on.")
     print("7. Actualizar entregas de alumnos v´ıa mail.")
     print("8. Salir.")
-    opcion=int()(input("Eliga una opcion:"))
+    opcion=validar_menu()   
 
+
+    if opcion==1:
+        print("1")
+    
+    elif opcion==2:
+        print("2")
+    
+    elif opcion==3:
+        print("3")
+    
+    elif opcion==4:
+        print("4")
+    
+    elif opcion==5:
+        print("5")
+    
+    elif opcion==6:
+        print("6")
+    
+    elif opcion==7:
+        print("7")

2a9a64933b3e49f2c66d5e74c6acdc7c31f86744
franlopez1234 <frlopez@fi.uba.ar>
2021-07-29 16:06:39 -0300

    Ultimas actualizaciones a todas las funciones

--- Funciones_de_Gogle_drive.py
+++ Funciones_de_Gogle_drive.py
@@ -6,7 +6,7 @@ import io
 from googleapiclient.http import MediaFileUpload
 from googleapiclient.http import MediaIoBaseDownload
 import pandas as pd 
-#import yaml
+import yaml
 
 def borrar_pantalla()->None:
     if os.name == "posix":
@@ -41,7 +41,7 @@ def create_remote_folder(folder_name:str)->None:
 def create_main_folder()->None:
     '''
     Pre: -
-    Post: Crea la carpeta principal del usuario en caso de no encontrarse 
+    Post: Crea la carpeta principal del usuario en caso de que no la tenga creada
     '''
     directory="C:\\Evaluaciones"
     try:
@@ -86,12 +86,14 @@ def select_extension()->str:
     Post: Valida que el formato este disponible
     '''
     diccionary_extensions = {'Texto':'.txt', 
-                            'Word':'.docx',}
+                            'Word':'.docx',
+                            'Power point':'.ppt',
+                            'Excel':'.xls'}
 
     borrar_pantalla()
     print("-------TIPOS DE EXTENSIONES DISPONIBLES-----------")
     for keys in diccionary_extensions:
-        print(keys) #BIBLIOTECA PARA MEJORAR EL PRINT DEL DICCIONARIO
+        print(yaml.dump(keys)) #BIBLIOTECA PARA MEJORAR EL PRINT DEL DICCIONARIO
 
     start = True
     while start :
@@ -106,7 +108,6 @@ def select_extension()->str:
         elif extension not in diccionary_extensions.keys():    #SI LA EXTENSION NO ESTA EN EL DIC QUE SIGA CICLANDO
             print("\nEsa extension no esta disponible\n")
 
-
 def create_remote_and_local_files()->None:
     '''
     Pre: Pide ingresar un nombre y un formato
@@ -115,12 +116,13 @@ def create_remote_and_local_files()->None:
     path = 'C:\Evaluaciones'  #GENERO LOS ARCHIVOS EN LA CARPETA PRINCIPAL DEL USUARIO
     start = True
     while start :
-        file_name = input ("\nIngrese el nombre del archivo que quiere crear:")
+        file_name = input ("\nIngrese el nombre del archivo que quiere:")
 
         if file_name !="":
             
             extension, mimetype = select_extension()
             file_format = file_name + extension
+            print(mimetype)
             create_remote_files(file_format, mimetype)
             
             while not os.path.exists(path):
@@ -129,7 +131,7 @@ def create_remote_and_local_files()->None:
             route =f'{path}' + f'\\{file_format}'
             archive = open(route, 'w')
             archive.close()
-    
+
             create_another_file = input ("\nQuieres crear otro archivo?(si/no):")
 
             if create_another_file !="si": start = False
@@ -143,25 +145,22 @@ def create_remote_files(file_name:str,mimeType:str):
     '''
     drive_service = service__drive.obtener_servicio()
 
-    file_metadata = {
-    'name' :f'{file_name}',
-    'mimeType' :f'{mimeType}'            
-    }                     
+    file_metadata = {'name' :f'{file_name}',
+                    'mimeType' :f'{mimeType}' }       
 
-    file = drive_service.files().create(body=file_metadata,
-                                        fields='id').execute()
-
+    file = drive_service.files().create(body=file_metadata, fields='id').execute()
 
 def select_mymetype(mimeType:str)->str:
     '''
     Pre: -
     Post: Returna la extension del archivo a crear
     '''
-    diccionary_mimeType = {'Texto':'.txt','Word':'application/vnd.google-apps.document',}
-    
+    diccionary_mimeType = {'Texto':'.txt',
+                            'Word':'application/vnd.google-apps.document',
+                            'Power point':'application/vnd.google-apps.presentation',
+                            'Excel':'application/vnd.google-apps.spreadsheet'}
     if mimeType in diccionary_mimeType.keys():
         mimeType = diccionary_mimeType[mimeType]#PIDO QUE LA EXTENSION ESTE EN EL DIC
-
     return mimeType
 
 def show_local_folders_and_files()->None:
@@ -179,7 +178,7 @@ def show_local_folders_and_files()->None:
     else:
         create_main_folder()
 
-def search_local_folders()->None:
+def search_local_folders()->str:
     list_folders = ['C:\Evaluaciones']
     directory = 'C:\Evaluaciones'
     
@@ -252,8 +251,8 @@ def show_remote_folders_and_files()->dict:
     service=service__drive.obtener_servicio()
     driveid = service.files().get(fileId='root').execute()['id'] #para obtener el drive id y poder listar solo "my-drive"
     
-    folder_id=f'{driveid}'
-    query=f"parents='{folder_id}' "
+    folder_id = f'{driveid}'
+    query = f"parents='{folder_id}' "
     
     response = service.files().list(q=query).execute()
     files = response.get('files')
@@ -262,7 +261,7 @@ def show_remote_folders_and_files()->dict:
     while nextPageToken:
         response=service.files(),list(q=query).execute()
         files.extend(response.get('nextPageToken'))
-        nextPageToken=response.get('nextPageToken')
+        nextPageToken = response.get('nextPageToken')
 
     change_format(files)
 
@@ -280,31 +279,29 @@ def folders_and_archives(response:None,dictionary:dict)->dict:
         id = file.get('id')  
 
         if id not in dictionary:
-            dictionary.update({name:id}) #EN EL CASO DE QUE EL DIC NO CONTENGA LOS ARCHIVOS LE HACEMOS UN UPDATE
-
+            dictionary.update({id:name}) #EN EL CASO DE QUE EL DIC NO CONTENGA LOS ARCHIVOS LE HACEMOS UN UPDATE
+                                #name:id hago una prueba
     return dictionary
 
-def search_in_remote_folders(diccionary_folders:dict)->None:
+def search_in_remote_folders(diccionary_folders:dict)->str:
     '''
     Pre: Pide el ingreso de datos
     Post: Revisa si la carpeta ingresada existe y muestra sus datos
     '''
-    service=service__drive.obtener_servicio()
-    continue_search=input("Quiere buscar en una carpeta?(si/no):")
+    service = service__drive.obtener_servicio()
+    continue_search=input("\nQuiere buscar una carpeta?(si/no):")
 
-    if continue_search=="si":
-        start=True
+    if continue_search=="si":start=True
 
-    else: 
-        start=False
+    else:
+        start=False 
+        folder_id = ""
 
     while start:
-        name_folder=input("Escriba el nombre de la carpeta que desea ver:")
-
-        if name_folder in diccionary_folders.keys():            
-            name_folder = diccionary_folders[name_folder]
-            folder_id = f'{name_folder}'
-            query = f"parents='{name_folder}'"
+        folder_id=input("\nEscriba el id de la carpeta (dicha id puede copiar y pegarse):")
+        print("\n")
+        if folder_id in diccionary_folders.keys():            
+            query = f"parents='{folder_id}'"
 
             response=service.files().list(q=query).execute()
             files = response.get('files')
@@ -317,17 +314,20 @@ def search_in_remote_folders(diccionary_folders:dict)->None:
                 nextPageToken = response.get('nextPageToken')
             change_format(files)
 
-        elif name_folder not in diccionary_folders.values():
+
+        elif folder_id not in diccionary_folders.keys():
             print("Esa carpeta no existe")
 
-        continue_search=input("Quiere buscar otra carpeta?(si/no):")
+        continue_search=input("\nQuiere seleccionar otra carpeta?(si/no):")
 
-        if continue_search=="si" and name_folder in diccionary_folders.values():
+        if continue_search=="si" and folder_id in diccionary_folders.keys():
             diccionary_folders = folders_and_archives(response,diccionary_folders) #LE SUMAMOS LOS ARCHIVOS AL DIC EN CADA INTERACCION
 
         elif continue_search!="si":
             start=False
-        
+
+    return folder_id
+
 def change_format(files)->None:
     '''
     Pre: -
@@ -342,73 +342,159 @@ def change_format(files)->None:
     df = pd.DataFrame(files)
     print(df)
 
-def dowload_files(diccionary_files:dict)->None:
+def download_remote_files(service:str, fileId:str, filePath:str)->None:
+    request = service.files().get_media(fileId=fileId)
+
+    fh = io.FileIO(filePath, mode='wb')
+    try:
+        downloader = MediaIoBaseDownload(fh, request, chunksize=1024*1024)
+        done = False
+        while done is False:
+            status, done = downloader.next_chunk(num_retries = 2)
+        if status:
+            print("Descargando %d%%." % int(status.progress() * 100))
+    finally:
+        fh.close()
+
+def download_folder(service, fileId, destinationFolder)->None:
+    if not os.path.isdir(destinationFolder):
+        os.mkdir(path=destinationFolder)
+
+    results = service.files().list(pageSize=300,q="parents in '{0}'".format(fileId),fields="files(id, name, mimeType)").execute()
+
+    items = results.get('files', [])
+
+    for item in items:
+        itemName = item['name']
+        itemId = item['id']
+        itemType = item['mimeType']
+        filePath = destinationFolder + "/" + itemName
+
+        if itemType == 'application/vnd.google-apps.folder': 
+            download_folder(service, itemId, filePath) 
+
+        elif itemType.startswith('application/vnd'):#SI ES UN ARCHIVO DE GOOGLE NO LO DESCARGAMOS DEBIDO A LOS ERRORES
+            print("Este tipo de archivos no pueden descargarse {0}".format(itemName))
+
+        elif not itemType.startswith('application/vnd.google-apps.folder'): #SI EL ARCHIVO NO ES UNA CARPETA LO DESCARGAMOS
+            download_remote_files(service, itemId, filePath)
+
+def download_file_request()->None:
     '''
     Pre: Pide el ingreso de datos
     Post: Descarga el archivo que seleccione el usuario 
     '''
+    download_another_file =""
+    continue_downloading = ""
+    done = True
     diccionary_files={}
-    diccionary_files = show_remote_folders_and_files()
-    service = service__drive.obtener_servicio() 
+    service = service__drive.obtener_servicio()
 
-    done= True
     while done:
         borrar_pantalla()
+        start = True
+
+        diccionary_files = show_remote_folders_and_files()
+        folder_id = search_in_remote_folders(diccionary_files)
+
+        while start:
+                file_id= input ("\nIngrese la id del archivo que quiere descargar (se puede copiar y pegar dicha id):")
+
+                if file_id in diccionary_files.keys() :
+                    file_name = diccionary_files[file_id]
+                    file_name = f'{file_name}'
+                    file_id = f'{file_id}'
+
+                    route = choose_dowload_location()
+                    filePath = route + f'\\{file_name}'
+                    download_remote_files(service, file_id, filePath) #LLAMAMOS A LA FUNCION PARA DESCARGAR UN ARCHIVO
+
+                else: 
+                    print("\nEse archivo no existe\n")
+                    download_another_file = input ("Quiere intentar descargar otro archivo de esta carpeta(si/no)?:")
+                if download_another_file != "si": 
+                    start = False
+                    continue_downloading = input("\nQuiere volver a la carpeta principal de descargas(si/no)?:")
+
+                if continue_downloading !="si": done = False  
+
+def download_folder_request():
+    diccionary_files ={}
+    service = service__drive.obtener_servicio()
+    folder_name = ""
+    continue_downloading = ""
+    download_another_folder = ""
+    done = True
+
+    while done :
+        borrar_pantalla()
+        start = True
         diccionary_files = show_remote_folders_and_files()
         search_in_remote_folders(diccionary_files)
 
-        file_to_download = input ("Ingrese el nombre del archivo que quiere descargar:")
-        file_name = f'{file_to_download}'
+        while start:
+            folder_id = input("\nIngrese el id de la carpeta que quiere descargar:")
 
-        if file_name in diccionary_files.keys() :
-            file_name = diccionary_files[file_name]
-            file_id = file_name
-            file_name = [f'{file_to_download}']
-            file_id = [f'{file_id}']
+            if folder_id in diccionary_files:
+                folde_name = diccionary_files[folder_id]
+                folder_id = f'{folder_id}'
+                
+                destinationFolder = choose_dowload_location() + f'\\{folder_name}'
+                download_folder(service, folder_id, destinationFolder)
+                download_another_folder= input("\nQuiere descargar otra carpeta? (si/no):")
 
-            for file_id, file_name in zip(file_id,file_name):
-                request = service.files().get_media(fileId = file_id)
-                fh = io.BytesIO()
-                dowloader = MediaIoBaseDownload(fd=fh , request = request)
-                results = choose_dowload_location() #FUNCION PARA DEFINIR LA LOCACION DONDE EL USUARION QUIERA DESCARGAR 
-    
+            else: 
+                print("\nNo selecciono correctamente la carpeta ")
+                download_another_folder= input("\nQuiere descargar otra carpeta? (si/no):")
+
+            if download_another_folder !="si":
                 start = False
-                while not start:
-                    status,start = dowloader.next_chunk()
-                    
+                continue_downloading = input("\nQuiere volver a la carpeta principal de descargas(si/no)?:")
 
-                    print("Descargar en proceso  {0}".format(status.progress() * 100)  )
-                    create_main_folder() #EN EL CASO DE QUE NUNCA SE HAYA CREADO UNA CARPETA PRINCIPAL, LA CREAMOS
-
-                fh.seek(0)
-                with open (os.path.join (f'{results}',file_name), 'wb' ) as f: #MODIFICO PARA PRUEBAS
-                    f.write(fh.read())
-                    f.close()
-
-        else: print("Ese archivo no existe")
-
-        continue_dowload = input ("Quiere descargar otro archivo (si/no)?:")
-
-        if continue_dowload != "si": done = False
-
-def choose_dowload_location():
+            if continue_downloading != "si": done = False
+            
+def choose_dowload_location()->str:
     borrar_pantalla()
-    print("\nSeleccione la carpeta donde desea descargar el archivo\n")
+    print("\n-Seleccione la carpeta donde desea descargar el archivo\n")
     show_local_folders_and_files()
     results = search_local_folders()
 
     return results
 
-def search_folder_upload():
-    search_in_folders = input ("Quieres buscar en alguna carpeta?:")
+def search_folder_upload()->str:
+    '''
+    Pre: Pide una serie de datos para la busqueda de carpetas
+    Post: Sube un archivo a la carepta elegida por el usuario
+    '''
+    diccionary_files = {}
+    folder_id = ""
+    service = service__drive.obtener_servicio()
 
-def prepare_to_upload(direction:str,file_name:str):
+    search_in_folders = input ("Quieres elegir una carpeta donde se suba el archivo (si/no)?:")
+
+    if search_in_folders =="si":
+        diccionary_files = show_remote_folders_and_files()
+        folder_id2 = search_in_remote_folders(diccionary_files)
+        folder_id = input ("Ingrese el id de la carpeta donde quiere subir el archivo:")
+    
+    elif folder_id in diccionary_files:
+        folder_name = diccionary_files[folder_id]
+        print(f"Archivo subido a la carpeta {folder_name}")
+
+    if folder_id == "" or folder_id not in diccionary_files or search_in_folders !="si":
+        folder_id =  service.files().get(fileId='root').execute()['id']
+        #SI LA ID LLEEGA VACIA O NO ESTA EN EL DICCIONARIO SUBE EL ARCHIVO DIRECTAMENTE A LA CARPETA PRINCIPAL DE DRIVE
+    
+    return folder_id
+
+
+def prepare_to_upload(direction:str,file_name:str)->None:
     '''
     Pre: -
     Post: Sube un archivo a la carpeta principal de google drive
     '''
     service = service__drive.obtener_servicio()
-    folder_id = service.files().get(fileId='root').execute()['id']
+    folder_id = search_folder_upload()
 
     file_metadata = {
         "name":f"{file_name}",             
@@ -417,7 +503,6 @@ def prepare_to_upload(direction:str,file_name:str):
     media = MediaFileUpload(f'{direction}', resumable=True)
     file = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
     
-        
 def select_file_to_upload()->str:
     start = True
     continue_upload = ""
@@ -434,7 +519,7 @@ def select_file_to_upload()->str:
 
         else: 
             print("\nError nombre del archivo incorrecto")
-            continue_upload = input ("\nQuiere intentar descargar otro archivo de esta carpeta (si/no)?:")
+            continue_upload = input ("\nQuiere intentarlo de nuevo (si/no)?:")
         
         if continue_upload == "no":
             start = False
@@ -445,12 +530,12 @@ def select_file_to_upload()->str:
 
     return direction, file_name
 
-def upload_files():
+def upload_files()->None:
     start = True
     while start:
         borrar_pantalla() 
         direction, file_name = select_file_to_upload() #FUNCION PARA BUSCAR EL ARCHIVO QUE DESEA SUBIR EL USUARIO
-      
+    
         if direction !="" and file_name !="":
             prepare_to_upload(direction, file_name)
             print("\nArchivo subio con exito")
@@ -474,10 +559,10 @@ def file_list_menu()->None:
 
     if opcion == 1:
         borrar_pantalla()
-        diccionary = {}
+        diccionary_files = {}
         print("---------Archivos que se encuentran en tu cuenta de gogle drive---------")
-        diccionary = show_remote_folders_and_files()
-        search_in_remote_folders(diccionary)
+        diccionary_files = show_remote_folders_and_files()
+        search_in_remote_folders(diccionary_files)
 
     elif opcion==2:
         borrar_pantalla()
@@ -489,21 +574,22 @@ def file_list_menu()->None:
 def dowload_menu()->None:
     print("-------OPCIONES------")
     print("1-Descargar archivos")
-    print("2-Salir ")
-    #print("3-Salir")
-    #print("2-Descargar carpetas")
-    print("")
-    range= 2
+    print("2-Descargar carpeta ")
+    print("3-Salir")
+    range= 3
     opcion=validar_rango(range)
 
     if opcion == 1:
         borrar_pantalla()
         diccionary_files = {}
-        dowload_files(diccionary_files)
+        download_file_request()
 
-    #elif opcion == 2:
+    elif opcion == 2:
+        borrar_pantalla()
+        print("-----------Descarga de carpetas-----------")
+        download_folder_request()
 
-def create_archives_menu():
+def create_archives_menu()->None:
     print("-------OPCIONES------")
     print("1-Crear una carpeta")
     print("2-Crear un archivo")
@@ -517,9 +603,10 @@ def create_archives_menu():
 
     elif opcion == 2:
         borrar_pantalla()
+        print("--------Crear archivos--------\n")
         create_remote_and_local_files()
     
-def upload_menu():
+def upload_menu()->None:
     print("-------OPCIONES------")
     print("1-Subir un archivo")
     print("2-Salir")
@@ -531,5 +618,7 @@ def upload_menu():
         upload_files()
 
 
-dowload_menu()
-1
+
+
+
+
